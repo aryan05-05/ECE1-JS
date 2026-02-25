ad7a6f37f3a1ac7c481b5638731c340e8d92eb24
Aryan Gupta <aryan89370@gmail.com>
2026-02-25 09:47:35 +0530

    data type

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log("Hello World");
 console.log("Welcome to JavaScript");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = "Hello";
+console.log(num);
+console.log(typeof(num));
+num = true;
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
