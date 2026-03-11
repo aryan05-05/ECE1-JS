fef4b8f337f6b0b5d3c74f4943d1085454810384
Aryan Gupta <aryan89370@gmail.com>
2026-03-11 09:43:58 +0530

    new

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 console.log("Hello World");
 console.log("Welcome to JavaScript");
-var num = 10;
-console.log(num);
-console.log(typeof(num));
-num = "Hello";
-console.log(num);
-console.log(typeof(num));
-num = true;
-console.log(num);
-console.log(typeof(num));
-num = null;
-console.log(num);
-console.log(typeof(num));
-num = undefined;
-console.log(num);
-console.log(typeof(num));
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Hello";
+// console.log(num);
+// console.log(typeof(num));
+// num = true;
+// console.log(num);
+// console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+// num = undefined;
+// console.log(num);
+// console.log(typeof(num));
+// const sym1=Symbol(4)// it is a unique identifier and it is immutable
+// console.log(sym1);
+// const sym2=Symbol(4);
+// console.log(sym2);
+// if(sym1===sym2){
+//     console.log("Symbols are equal");
+// }
+// else{
+//     console.log("Symbols are not equal");
+// }
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+//syntax of for loop
+//for(key in object){
+//}
+// const person={
+//     fname:"Ajay",
+//     iname:"Badmash",
+//     age:19
+// };
+// let x;
+// for(x in person){
+//     console.log("person details:"+x+":"+person[x])
+// }
+
+// let y=0;
+// while(y<10){
+//     console.log(x);
+//     y++;
+// }
