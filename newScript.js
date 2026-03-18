1103f0cf2c7f77297996942cb9fa3619b25f407d
Aryan Gupta <aryan89370@gmail.com>
2026-03-18 10:23:21 +0530

    DOM element

--- newScript.js
+++ newScript.js
@@ -6,5 +6,36 @@
 // result2[1].innerHTML="<b>Hello World</b>"
 // result2[1].style.color="red"
 // result2[1].style.fontSize="40px"
-const result3=document.getElementById("ECE1").getAttribute("class");
-const x=document.getElementById("p1").innerHTML="result3"
+// const result3=document.getElementById("ECE1").getAttribute("class");
+// const x=document.getElementById("p1").innerHTML="result3"
+
+function onClick(){
+    console.log("Clicked");
+}
+
+function onMouseover(){
+    console.log("Mouse Over");
+}
+
+function onMouseout(){
+    console.log("Mouse Out");
+}
+
+function onMousedown(){
+    console.log("Mouse Down");
+}
+
+function onMouseup(){
+    console.log("Mouse Up");
+}
+
+function onMousemove(){
+    console.log("Mouse Move");
+}
+
+function onMousedoubleclick(){
+    console.log("Mouse Double Click");
+}
+function myFunction(){
+    alert("Window loaded successfully!");
+}
