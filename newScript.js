7e0976445ecef9dc6e0ec8c26ccf252e64193643
Aryan Gupta <aryan89370@gmail.com>
2026-03-18 09:49:56 +0530

    DOM

---
+++ newScript.js
@@ -0,0 +1,10 @@
+// const result=document.getElementById("aG")
+// console.log(result);
+// result.innerHTML="<b>Hello World</b>"
+// const result2=document.getElementsByClassName("aG")
+// // console .log(result2);
+// result2[1].innerHTML="<b>Hello World</b>"
+// result2[1].style.color="red"
+// result2[1].style.fontSize="40px"
+const result3=document.getElementById("ECE1").getAttribute("class");
+const x=document.getElementById("p1").innerHTML="result3"
